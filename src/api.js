f7200234c56341ea8bea10df1e349000bc78d2ff
Sahan Pasindu <sahanpasindu07@gmail.com>
2021-03-12 16:15:42 +0530

    2. Setup API and pipular game url

---
+++ src/api.js
@@ -0,0 +1,36 @@
+// BASE URL
+// cd4188f8d7124ddea2a63e6dab84e6ed
+const base_url = "https://api.rawg.io/api/";
+
+// getting the date
+const getCurrentMonth = () => {
+  const month = new Date().getMonth() + 1;
+  if (month < 10) {
+    return `0${month}`;
+  } else {
+    return month;
+  }
+};
+
+const getCurrentDay = () => {
+  const day = new Date().getDate() + 1;
+  if (day < 10) {
+    return `0${day}`;
+  } else {
+    return day;
+  }
+};
+
+// current day/month/year
+const currentYear = new Date().getFullYear();
+const currentMonth = getCurrentMonth();
+const currentDay = getCurrentDay();
+
+const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
+const lastYear = `${currentYear - 1}-${currentMonth}-${currentDate}`;
+const nextYear = `${currentYear + 1}-${currentMonth}-${currentDate}`;
+
+// Popular Games (from last year this day)
+const popular_games = `games?dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
+
+export const popularGamesURL = () => `${base_url}${popular_games}`;
