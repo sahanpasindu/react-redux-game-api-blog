16903d1b6be43be45f66395ac01e65b05916efa8
Sahan Pasindu <sahanpasindu07@gmail.com>
2021-03-13 10:33:46 +0530

    6. Fetch other games

--- src/actions/gamesAction.js
+++ src/actions/gamesAction.js
@@ -1,16 +1,20 @@
 import axios from "axios";
-import { popularGamesURL } from "../api";
+import { popularGamesURL, newGamesURL, upcomingGamesURL } from "../api";
 
 // action creator
 export const loadGames = () => async (dispatch) => {
   // fetch with axios
   // console.log(popularGamesURL());
   const popularData = await axios.get(popularGamesURL());
+  const newGameData = await axios.get(newGamesURL());
+  const upcomingData = await axios.get(upcomingGamesURL());
 
   dispatch({
     type: "FETCH_GAMES",
     payload: {
       popular: popularData.data.results,
+      upcoming: upcomingData.data.results,
+      newGames: newGameData.data.results,
     },
   });
 };
