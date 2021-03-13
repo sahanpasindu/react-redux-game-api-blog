1301c71da9fe934299a7f00ed54f18bf38f74a36
Sahan Pasindu <sahanpasindu07@gmail.com>
2021-03-13 14:02:45 +0530

    8. Styling up cards 01

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,57 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+// redux
+import { useDispatch, useSelector } from "react-redux";
 import { loadGames } from "../actions/gamesAction";
+
+// components
+import Game from "../components/Game";
+
+// styles
+import styled from "styled-components";
+import { motion } from "framer-motion";
 
 const Home = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(loadGames());
-  });
+  }, [dispatch]);
+
+  // get that data back
+  const { popular, newGames, upcoming } = useSelector((state) => state.games);
+  console.log(popular);
   return (
-    <div className="home">
-      <h1>Hello</h1>
-    </div>
+    <GameList className="home">
+      <h2>Upcoming Games</h2>
+      <Games>
+        {upcoming.map((game) => (
+          <Game
+            name={game.name}
+            released={game.released}
+            id={game.id}
+            image={game.background_image}
+            key={game.id}
+          ></Game>
+        ))}
+      </Games>
+    </GameList>
   );
 };
+
+const GameList = styled(motion.div)`
+  padding: 0rem 5rem;
+
+  h2 {
+    padding: 5rem 0rem;
+  }
+`;
+
+const Games = styled(motion.div)`
+  min-height: 80vh;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(500px, 1fr));
+  grid-column-gap: 3rem;
+  grid-row-gap: 5rem;
+`;
 
 export default Home;
